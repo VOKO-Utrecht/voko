fdb63b5c422efecffcf2f1c3fea4f22882e4ca3c
Rik <rvachterberg@gmail.com>
2014-07-26 20:47:41 +0200

    Only fetch products of current order round

--- webapp/ordering/core.py
+++ webapp/ordering/core.py
@@ -3,4 +3,5 @@ from ordering.models import OrderRound
 
 def get_current_order_round():
     ## TODO: Get current order round based on current date
+    ## If non existing, get_or_create?
     return list(OrderRound.objects.all())[-1]
--- webapp/ordering/views.py
+++ webapp/ordering/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
+from ordering.core import get_current_order_round
 from ordering.models import Product
 
 
 class ProductsView(ListView):
-    model = Product
+    queryset = Product.objects.filter(order_round=get_current_order_round())
