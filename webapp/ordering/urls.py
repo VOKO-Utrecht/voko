685041ab3a3596d29a0f36fa04fd652e2773fb62
Rik <rvachterberg@gmail.com>
2014-07-28 20:03:28 +0200

    Placeholder product detail view

--- webapp/ordering/urls.py
+++ webapp/ordering/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import ProductsView
+from .views import ProductsView, ProductView
 
 urlpatterns = patterns('',
     url(r'^products/', ProductsView.as_view()),
+    url(r'^product/(?P<pk>[0-9]+)/$', ProductView.as_view()),
 )
--- webapp/ordering/views.py
+++ webapp/ordering/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from ordering.core import get_current_order_round
 from ordering.models import Product
 
 
 class ProductsView(ListView):
     queryset = Product.objects.filter(order_round=get_current_order_round())
+
+
+class ProductView(DetailView):
+    model = Product
