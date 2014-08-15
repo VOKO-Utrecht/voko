fd7082bfdd00d64b0852cfb27ea55fb44c52c3f3
Rik <rvachterberg@gmail.com>
2014-08-15 11:54:48 +0200

    User is by default false - email should be validated

--- webapp/accounts/models.py
+++ webapp/accounts/models.py
@@ -53,7 +53,7 @@ class VokoUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
 
     REQUIRED_FIELDS = ["first_name", "last_name"]
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     objects = VokoUserManager()
