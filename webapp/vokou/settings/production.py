f7e78fffedce7b46d0fecc51aa5626d9a81110a4
Rik <rvachterberg@gmail.com>
2014-08-29 11:41:06 +0200

    Deployment DB settings

--- webapp/vokou/settings/production.py
+++ webapp/vokou/settings/production.py
@@ -5,9 +5,9 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'voko',                      # Or path to database file if using sqlite3.
-        'USER': 'voko',
-        # 'PASSWORD': 'password',
+        'NAME': 'vokou',                      # Or path to database file if using sqlite3.
+        'USER': 'vokou',
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         }
     }
