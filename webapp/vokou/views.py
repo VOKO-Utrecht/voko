c388cca05906416ff553a51ea85be41c6b79a6a4
Rik <rvachterberg@gmail.com>
2014-09-01 21:40:29 +0200

    Removed header for now. Created redirect to vokoutrecht.

--- webapp/vokou/views.py
+++ webapp/vokou/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.views.generic import View
 
 
 class HomeView(View):
     def get(self, *args, **kwargs):
-        return redirect('view_products')
+        # return redirect('view_products')
+        return HttpResponseRedirect("http://vokoutrecht.nl/")
