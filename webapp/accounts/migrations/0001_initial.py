43b2c6eb904eee60e364a361964e96cd885eb0bf
Rik <rvachterberg@gmail.com>
2014-09-01 22:08:42 +0200

    Added migrations for accounts app

---
+++ webapp/accounts/migrations/0001_initial.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='VokoUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('email', models.EmailField(unique=True, max_length=255, verbose_name=b'E-mail adres', db_index=True)),
+                ('first_name', models.CharField(max_length=30, verbose_name='Voornaam')),
+                ('last_name', models.CharField(max_length=30, verbose_name='Achternaam')),
+                ('can_activate', models.BooleanField(default=False)),
+                ('is_active', models.BooleanField(default=False)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('is_staff', models.BooleanField(default=False)),
+                ('groups', models.ManyToManyField(to='auth.Group', verbose_name='groups', blank=True)),
+                ('user_permissions', models.ManyToManyField(to='auth.Permission', verbose_name='user permissions', blank=True)),
+            ],
+            options={
+                'ordering': (b'-modified', b'-created'),
+                'abstract': False,
+                'get_latest_by': b'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('street_and_number', models.CharField(max_length=100, blank=True)),
+                ('zip_code', models.CharField(max_length=7)),
+                ('city', models.CharField(max_length=100, blank=True)),
+            ],
+            options={
+                'ordering': (b'-modified', b'-created'),
+                'abstract': False,
+                'get_latest_by': b'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='EmailConfirmation',
+            fields=[
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('token', models.CharField(max_length=100, serialize=False, primary_key=True)),
+                ('is_confirmed', models.BooleanField(default=False)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': (b'-modified', b'-created'),
+                'abstract': False,
+                'get_latest_by': b'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('notes', models.TextField()),
+                ('address', models.ForeignKey(to='accounts.Address')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': (b'-modified', b'-created'),
+                'abstract': False,
+                'get_latest_by': b'modified',
+            },
+            bases=(models.Model,),
+        ),
+    ]
