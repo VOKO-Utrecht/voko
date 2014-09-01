f441829cd854e4785a2d8e92cc7b9ae6a1fe93d0
Rik <rvachterberg@gmail.com>
2014-09-01 21:20:28 +0200

    Streamlining registration and activation

--- webapp/accounts/urls.py
+++ webapp/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import LoginView, RegisterView, PasswordResetView, EmailConfirmView
+from .views import LoginView, RegisterView, PasswordResetView, EmailConfirmView, FinishRegistration, RegisterThanksView
 
 urlpatterns = patterns('',
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^register/$', RegisterView.as_view(), name="register"),
+    url(r'^register/thanks/$', RegisterThanksView.as_view(), name="register_thanks"),
     url(r'^register/confirm/(?P<pk>[0-9a-zA-Z\-]+)$', EmailConfirmView.as_view(), name="confirm_email"),
+    url(r'^register/finish/(?P<pk>[0-9a-zA-Z\-]+)$', FinishRegistration.as_view(), name="finish_registration"),
     url(r'^passwordreset/$', PasswordResetView.as_view()),
 )
--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
-from django.shortcuts import redirect
-from django.views.generic import FormView, DetailView
-from accounts.forms import VokoUserCreationForm
-from accounts.models import EmailConfirmation
+from django.http import Http404
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import FormView, DetailView, UpdateView, TemplateView
+from accounts.forms import VokoUserCreationForm, VokoUserFinishForm
+from accounts.models import EmailConfirmation, VokoUser
 from django.conf import settings
 
 
@@ -19,7 +20,7 @@ class LoginView(FormView):
 class RegisterView(FormView):
     template_name = "accounts/register.html"
     form_class = VokoUserCreationForm
-    success_url = "/todo"
+    success_url = "/accounts/register/thanks"  # TODO: Reverse url
 
     def get_context_data(self, **kwargs):
         context = super(FormView, self).get_context_data(**kwargs)
@@ -31,6 +32,27 @@ class RegisterView(FormView):
         user = form.save()
         user.email_confirmation.send_confirmation_mail()
         return super(RegisterView, self).form_valid(form)
+
+
+class RegisterThanksView(TemplateView):
+    template_name = "accounts/register_thanks.html"
+
+
+class FinishRegistration(UpdateView):
+    template_name = "accounts/activate.html"
+    success_url = "/accounts/welcome"
+    form_class = VokoUserFinishForm
+
+    def get_queryset(self):
+        # We re-use the email confirmation token.
+        return EmailConfirmation.objects.filter(is_confirmed=True, user__can_activate=True,
+                                                user__is_active=False, token=self.kwargs['pk'])
+
+    def get_object(self, queryset=None):
+        try:
+            return self.get_queryset()[0].user
+        except IndexError:
+            raise Http404
 
 
 class PasswordResetView(FormView):
