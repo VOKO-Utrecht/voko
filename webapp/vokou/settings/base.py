45ef831fc5d55305d674924c86d435c80e7c102f
Rik <rvachterberg@gmail.com>
2014-09-13 16:24:29 +0200

    Added django.middleware.common.BrokenLinkEmailsMiddleware to settings

--- webapp/vokou/settings/base.py
+++ webapp/vokou/settings/base.py
@@ -40,6 +40,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
 )
 
 ROOT_URLCONF = 'vokou.urls'
