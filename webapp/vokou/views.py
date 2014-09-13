2112a9c6f6c9eb72306aa08e09b5f98080a7b752
Rik <rvachterberg@gmail.com>
2014-09-13 10:30:28 +0200

    Removed redirect

--- webapp/vokou/views.py
+++ webapp/vokou/views.py
@@ -1,9 +1,7 @@
-from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.views.generic import View
 
 
 class HomeView(View):
     def get(self, *args, **kwargs):
-        # return redirect('view_products')
-        return HttpResponseRedirect("http://vokoutrecht.nl/")
+        return redirect('view_products')
