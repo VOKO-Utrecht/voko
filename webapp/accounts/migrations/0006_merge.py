d692f76f9f2cb16243477a74827405b31065a75d
Rik <rvachterberg@gmail.com>
2014-09-15 12:01:08 +0200

    Getting tired of these fucking migrations

--- webapp/accounts/migrations/0006_merge.py
+++ webapp/accounts/migrations/0006_merge.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0005_auto_20140915_0932'),
         ('accounts', '0002_auto_20140915_0934'),
     ]
 
