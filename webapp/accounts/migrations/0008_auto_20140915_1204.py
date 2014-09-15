6b1f2b8b58b7bf85fb058c5d8c9760f050d6ef4c
Rik <rvachterberg@gmail.com>
2014-09-15 12:05:09 +0200

    Migrations bs

---
+++ webapp/accounts/migrations/0008_auto_20140915_1204.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0007_auto_20140915_1118'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='address',
+            options={'verbose_name': b'adres', 'verbose_name_plural': b'adressen'},
+        ),
+        migrations.AlterModelOptions(
+            name='emailconfirmation',
+            options={'verbose_name': b'emailbevestiging', 'verbose_name_plural': b'emailbevestigingen'},
+        ),
+        migrations.AlterModelOptions(
+            name='userprofile',
+            options={'verbose_name': b'ledenprofiel', 'verbose_name_plural': b'ledenprofielen'},
+        ),
+        migrations.AlterModelOptions(
+            name='vokouser',
+            options={'verbose_name': b'lid', 'verbose_name_plural': b'leden'},
+        ),
+    ]
