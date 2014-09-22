506e7a1ce639415042e193c314c61fc1c168dc30
Rik <rvachterberg@gmail.com>
2014-09-22 21:57:56 +0200

    Order by modified date

--- webapp/ordering/views.py
+++ webapp/ordering/views.py
@@ -212,4 +212,4 @@ class UserOrdersPerOrderRoundView(StaffuserRequiredMixin, ListView):
 
     def get_queryset(self):
         order_round = OrderRound.objects.get(pk=self.kwargs.get('pk'))
-        return Order.objects.filter(order_round=order_round, finalized=True).order_by("pk")
+        return Order.objects.filter(order_round=order_round, finalized=True).order_by("modified")
