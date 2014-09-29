ae6a6b4d79629ee8ca2823870072f11ade7d65c0
Rik <rvachterberg@gmail.com>
2014-09-29 07:49:58 +0200

    Shit. temp disablead clean() in form because of bug

--- webapp/ordering/forms.py
+++ webapp/ordering/forms.py
@@ -8,20 +8,20 @@ class OrderProductForm(forms.ModelForm):
         fields = ['amount', 'product']
         widgets = {'product': forms.HiddenInput()}
 
-    def clean(self):
-        cleaned_data = super(OrderProductForm, self).clean()
-        amount = cleaned_data.get('amount')
-
-        if amount < 1:
-            raise forms.ValidationError("Geen valide getal.")
-
-        # When called from product detail view, check for max amount
-        if 'product' in self.cleaned_data:
-            product = cleaned_data.get('product')
-            if amount > product.amount_available:
-                raise forms.ValidationError("Dit aantal is niet beschikbaar.")
-
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = super(OrderProductForm, self).clean()
+    #     amount = cleaned_data.get('amount')
+    #
+    #     if amount < 1:
+    #         raise forms.ValidationError("Geen valide getal.")
+    #
+    #     # When called from product detail view, check for max amount
+    #     if 'product' in self.cleaned_data:
+    #         product = cleaned_data.get('product')
+    #         if amount > product.amount_available:
+    #             raise forms.ValidationError("Dit aantal is niet beschikbaar.")
+    #
+    #     return cleaned_data
 
 
 class OrderForm(forms.ModelForm):
