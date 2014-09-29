2e63d3ce1e05cce7cb4323a1e36421f65e9a5518
Rik <rvachterberg@gmail.com>
2014-09-29 14:30:13 +0200

    Update admin.py

--- webapp/ordering/admin.py
+++ webapp/ordering/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.db.models.loading import get_models, get_app
+from .models import Order, OrderProduct
 
 for model in get_models(get_app('ordering')):
+    if model == Order:
+        continue
     admin.site.register(model)
+    
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderProductInline,
+    ]
+    
+admin.site.register(Order, OrderAdmin)
