7fc659a6dd6edbb937e18f2234881cd75e1ec754
Rik <rvachterberg@gmail.com>
2014-10-03 16:00:07 +0200

    Updated gunicorn.sh

--- webapp/gunicorn.sh
+++ webapp/gunicorn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-DJANGODIR=/home/ubuntu/app
 DJANGO_SETTINGS_MODULE=vokou.settings.production
 
 USER=voko
@@ -11,7 +10,5 @@ cd /home/voko/vokou/webapp
 source /home/voko/.virtualenvs/voko/bin/activate
 
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn vokou.wsgi:application -c /home/voko/vokou/webapp/gunicorn.conf.py
