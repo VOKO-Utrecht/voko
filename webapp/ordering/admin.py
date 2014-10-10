66ea45ab2d5e1ec34d3eff82d38f93a532bfad3d
Rik <rvachterberg@gmail.com>
2014-10-10 17:23:38 +0200

    Ordering: improving admin

--- webapp/ordering/admin.py
+++ webapp/ordering/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db.models.loading import get_models, get_app
+from finance.models import Balance
 from .models import Order, OrderProduct
 
 for model in get_models(get_app('ordering')):
@@ -12,10 +13,27 @@ class OrderProductInline(admin.TabularInline):
     model = OrderProduct
 
 
+def set_order_collected(modeladmin, request, queryset):
+    for order in queryset:
+        order.collected = True
+        order.save()
+set_order_collected.short_description = "Bestelling opgehaald"
+
+
+def create_credit_for_order(modeladmin, request, queryset):
+    for order in queryset:
+        Balance.objects.create(user=order.user,
+                               type="CR",
+                               amount=order.total_price,
+                               notes="Bestelling #%d contant betaald" % order.pk)
+create_credit_for_order.short_description = "Contant betaald"
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["id", "order_round", "user", "finalized", "collected", "total_price"]
     ordering = ("-id", )
     inlines = [OrderProductInline]
     list_filter = ("finalized", "collected")
+    actions = (create_credit_for_order, set_order_collected)
 
 admin.site.register(Order, OrderAdmin)
