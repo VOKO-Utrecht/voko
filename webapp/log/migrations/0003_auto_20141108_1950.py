7a1dc4f80ef99ef4766b2d36fa37c1aa2d062be3
Rik <rvachterberg@gmail.com>
2014-11-08 19:57:19 +0100

    Set operator field on eventlog to optional

---
+++ webapp/log/migrations/0003_auto_20141108_1950.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('log', '0002_auto_20141011_2146'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='eventlog',
+            name='operator',
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+    ]
