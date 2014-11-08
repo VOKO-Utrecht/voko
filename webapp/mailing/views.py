dea9c803c4050bd37e04adcd4c5cdc9fd89faeb4
Rik <rvachterberg@gmail.com>
2014-11-08 20:10:45 +0100

    Bugfix: Don't unpack kwargs

--- webapp/mailing/views.py
+++ webapp/mailing/views.py
@@ -13,7 +13,7 @@ import html2text
 def render_mail_template(template, **kwargs):
     body_tpl = Template(template.html_body)
     subject_tpl = Template(template.subject)
-    context = Context(**kwargs)
+    context = Context(kwargs)
 
     rendered_subject = subject_tpl.render(context)
     rendered_html_body = body_tpl.render(context)
