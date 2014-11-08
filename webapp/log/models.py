9a79a5bfc1b77f82511b4e8bf18a1f6e4f8f1f2a
Rik <rvachterberg@gmail.com>
2014-11-08 19:58:20 +0100

    Set operator field on eventlog to optional

--- webapp/log/models.py
+++ webapp/log/models.py
@@ -4,7 +4,7 @@ from accounts.models import VokoUser
 
 
 class EventLog(TimeStampedModel):
-    operator = models.ForeignKey(VokoUser, related_name="operator_logs")
+    operator = models.ForeignKey(VokoUser, related_name="operator_logs", null=True, blank=True)
     user = models.ForeignKey(VokoUser, null=True, blank=True, related_name="user_logs")
     event = models.CharField(max_length=255)
     extra = models.TextField(blank=True, null=True)
