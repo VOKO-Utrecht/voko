5265bc5d21981618c021a0b45b064c36f13f5331
Rik <rvachterberg@gmail.com>
2014-11-08 19:56:39 +0100

    Set operator field on eventlog to optional

---
+++ webapp/ordering/migrations/0017_auto_20141108_1950.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ordering', '0016_auto_20141108_1139'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='order',
+            options={'verbose_name': b'Bestelling', 'verbose_name_plural': b'Bestellingen'},
+        ),
+        migrations.AlterModelOptions(
+            name='orderproduct',
+            options={'verbose_name': b'Productbestelling', 'verbose_name_plural': b'Productbestellingen'},
+        ),
+        migrations.AlterModelOptions(
+            name='orderproductcorrection',
+            options={'verbose_name': b'Productbestelling-correctie', 'verbose_name_plural': b'Productbestelling-correcties'},
+        ),
+        migrations.AlterModelOptions(
+            name='orderround',
+            options={'verbose_name': b'Bestelronde', 'verbose_name_plural': b'Bestelronden'},
+        ),
+        migrations.AlterModelOptions(
+            name='product',
+            options={'verbose_name': b'Product', 'verbose_name_plural': b'Producten'},
+        ),
+        migrations.AlterModelOptions(
+            name='supplier',
+            options={'verbose_name': b'Leverancier'},
+        ),
+    ]
