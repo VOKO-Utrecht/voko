e745cb25b544e70486b08d0499bcb2cebd854694
Rik <rvachterberg@gmail.com>
2014-11-21 18:10:09 +0100

    Finance model tests

---
+++ webapp/finance/tests/test_models.py
@@ -0,0 +1,50 @@
+import mock
+from unittest import TestCase
+from accounts.models import VokoUser
+from finance.models import Balance
+
+
+# TODO move to root app
+class VokoTestCase(TestCase):
+    def patch(self, to_patch):
+        patcher = mock.patch(to_patch)
+        self.addCleanup(patcher.stop)
+        return patcher.start()
+
+
+class TestBalanceManager(VokoTestCase):
+    def setUp(self):
+        self.vokouser = VokoUser.objects.create(email="test@example.com",
+                                                first_name="John",
+                                                last_name="Doe")
+
+    def tearDown(self):
+        self.vokouser.delete()
+
+    def test_methods_on_user_object_via_balance_model(self):
+        self.vokouser.balance.credit()
+        self.vokouser.balance.debit()
+
+    def test_simple_credit(self):
+        Balance.objects.create(user=self.vokouser,
+                               type="CR",
+                               amount=11.11)
+        self.assertEqual(float(self.vokouser.balance.credit()), 11.11)
+        self.assertEqual(self.vokouser.balance.debit(), 0)
+
+    def test_simple_debit(self):
+        Balance.objects.create(user=self.vokouser,
+                               type="DR",
+                               amount=22.22)
+        self.assertEqual(float(self.vokouser.balance.debit()), 22.22)
+        self.assertEqual(self.vokouser.balance.credit(), 0)
+
+    def test_that_debit_and_credit_even_each_other_out(self):
+        self.vokouser.balance.create(type="CR", amount=12)
+        self.vokouser.balance.create(type="CR", amount=0.99)
+        self.vokouser.balance.create(type="DR", amount=12)
+        self.vokouser.balance.create(type="DR", amount=0.99)
+
+        self.assertEqual(self.vokouser.balance.credit(), 0)
+        self.assertEqual(self.vokouser.balance.debit(), 0)
+
