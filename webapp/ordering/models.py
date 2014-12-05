0ff89729c5df5e0cd3b114665093f685e52aef2d
Rik <rvachterberg@gmail.com>
2014-12-05 14:38:48 +0100

    Added half pound to product units

--- webapp/ordering/models.py
+++ webapp/ordering/models.py
@@ -212,6 +212,7 @@ class Product(TimeStampedModel):
         ('Gram',  'Gram'),
         ('Decagram', 'Decagram (10g)'),
         ('Hectogram', 'Hectogram (100g)'),
+        ('Half pond', 'Halve pond (250g)'),
         ('Pond',  'Pond (500g)'),
         ('Kilogram', 'Kilogram'),
         ('Deciliter', 'Deciliter (100ml)'),
