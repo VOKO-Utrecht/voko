04673058de2406abe9d0f79116e796a77325e26d
Rik van Achterberg <rik@byte.nl>
2015-02-02 09:35:24 +0100

    Fixed int-casting bug

--- webapp/ordering/views.py
+++ webapp/ordering/views.py
@@ -58,6 +58,8 @@ class ProductsView(LoginRequiredMixin, ListView):
                     value = 0
 
                 # User deleted a product
+                if not value.isdigit():
+                    value = 0
                 if not int(value):
                     if order_product:
                         order_product.delete()
