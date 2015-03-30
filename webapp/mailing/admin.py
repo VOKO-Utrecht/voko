09f5298a333fe9ddd2150a39f1ec7404e45722ad
Rik van Achterberg <rik@byte.nl>
2015-03-30 18:53:31 +0200

    Nicer mailtemplate admin

--- webapp/mailing/admin.py
+++ webapp/mailing/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from mailing.models import MailTemplate
 
 
-admin.site.register(MailTemplate)
+class MailTemplateAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "subject", "created", "modified"]
+    ordering = ("-id", "modified")
+
+admin.site.register(MailTemplate, MailTemplateAdmin)
+
