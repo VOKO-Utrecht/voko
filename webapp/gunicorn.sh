77ed559ed84c04d80391202c62b651f1d06afda8
Rik van Achterberg <rik@byte.nl>
2015-05-03 17:40:13 +0200

    updated gunicorn start file with default config keys

--- webapp/gunicorn.sh
+++ webapp/gunicorn.sh
@@ -10,5 +10,11 @@ cd /home/voko/vokou/webapp
 source /home/voko/.virtualenvs/voko/bin/activate
 
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export DJANGO_SECRET_KEY=""
+export DB_PASSWORD=""
+export QANTANI_MERCHANT_ID=""
+export QANTANI_MERCHANT_KEY=""
+export QANTANI_MERCHANT_SECRET=""
+
 
 exec gunicorn vokou.wsgi:application -c /home/voko/vokou/webapp/gunicorn.conf.py
