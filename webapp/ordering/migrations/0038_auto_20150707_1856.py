0272d5abf7d961b9fbc231944a603a19ee48fdaa
Rik van Achterberg <rik@byte.nl>
2015-07-07 18:57:05 +0200

    Migration for the Read Only user model

---
+++ webapp/ordering/migrations/0038_auto_20150707_1856.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ordering', '0037_remove_orderround_suppliers_reminder_sent'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='orderround',
+            name='order_placed',
+            field=models.BooleanField(default=False, editable=False),
+            preserve_default=True,
+        ),
+    ]
