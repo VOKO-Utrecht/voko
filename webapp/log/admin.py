cb0936310cf5f792fa76edf9796be19916cd07d2
Rik van Achterberg <rik@byte.nl>
2015-08-22 16:35:55 +0200

    Make eventlogs searchable

--- webapp/log/admin.py
+++ webapp/log/admin.py
@@ -5,5 +5,6 @@ from log.models import EventLog
 class EventLogAdmin(admin.ModelAdmin):
     list_display = ["created", "event", "operator", "user"]
     ordering = ("-id", )
+    search_fields = ['event', 'operator__email', 'user__email']
 
 admin.site.register(EventLog, EventLogAdmin)
