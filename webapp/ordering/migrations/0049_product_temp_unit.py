a1f995978dbbc48799cc8037c98c8e9b634456e0
Rik van Achterberg <rik@byte.nl>
2015-10-18 14:43:49 +0200

    Add temp_unit field to Product for our data migration

---
+++ webapp/ordering/migrations/0049_product_temp_unit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ordering', '0048_auto_20151018_1422'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='product',
+            name='temp_unit',
+            field=models.ForeignKey(to='ordering.ProductUnit', null=True),
+            preserve_default=True,
+        ),
+    ]
