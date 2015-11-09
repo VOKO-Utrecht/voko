76e88cf0f8a4bdd7b4a93a9002932e59506c272d
Rik van Achterberg <rik@byte.nl>
2015-11-09 13:12:34 +0100

    Remove obsolete view and redirect to new view

--- webapp/finance/admin_views.py
+++ webapp/finance/admin_views.py
@@ -6,50 +6,6 @@ import simplejson as json  # Decimal support
 from collections import OrderedDict
 
 
-class RoundsPerYearView(GroupRequiredMixin, ListView):
-    template_name = "finance/admin/specified.html"
-    group_required = "Admin"
-
-    def get_queryset(self):
-        # return order rounds that *opened* in $year
-        return OrderRound.objects.filter(open_for_orders__year=self.kwargs['year']).order_by('-id')
-
-    def get_context_data(self, **kwargs):
-        context = super(RoundsPerYearView, self).get_context_data(**kwargs)
-        context['year'] = self.kwargs['year']
-        context['suppliers_data'] = self.compose_table_data()
-        context['totals'] = self.get_order_totals_per_round()
-        return context
-
-    def compose_table_data(self):
-        ret = []
-
-        for rnd in self.get_queryset():
-            for supplier in rnd.suppliers():
-                d = dict()
-                d['round'] = rnd
-                d['supplier'] = supplier
-                d['total_amount'] = rnd.total_order_amount_per_supplier(supplier)
-
-                corrections = OrderProductCorrection.objects.filter(order_product__order__order_round=rnd,
-                                                                    order_product__product__supplier=supplier)
-                d['supplier_corrections_exc'] = sum([c.calculate_supplier_refund()
-                                                     for c in corrections.filter(charge_supplier=True)])
-                # d['supplier_corrections_inc'] = sum([c.calculate_refund()
-                #                                      for c in corrections.filter(charge_supplier=True)])
-                d['voko_corrections_inc'] = sum([c.calculate_refund()
-                                                for c in corrections.filter(charge_supplier=False)])
-
-                d['to_pay'] = d['total_amount'] - d['supplier_corrections_exc']
-                ret.append(d)
-
-        return ret
-
-    def get_order_totals_per_round(self):
-        for rnd in self.get_queryset():
-            yield {rnd: rnd.total_order_amount()}
-
-
 class JsonRoundOverview(GroupRequiredMixin, View):
     group_required = "Admin"
 
--- webapp/finance/urls.py
+++ webapp/finance/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 import views
 import admin_views
 
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^pay/transaction/create/$', views.CreateTransactionView.as_view(), name="finance.createtransaction"),
     url(r'^pay/transaction/confirm/$', views.ConfirmTransactionView.as_view(), name="finance.confirmtransaction"),
     url(r'^pay/transaction/callback/$', views.QantaniCallbackView.as_view(), name="finance.callback"),
-    url(r'^admin/(?P<year>[0-9]+)/specified/$', admin_views.RoundsPerYearView.as_view(), name="finance.admin.specified"),
+    url(r'^admin/(?P<year>[0-9]+)/specified/$', RedirectView.as_view(pattern_name='finance.admin.year.overview')),
     url(r'^admin/json/round/(?P<round_id>[0-9]+)/$', admin_views.JsonRoundOverview.as_view(), name="finance.admin.round.overview.json"),
     url(r'^admin/round/(?P<round_id>[0-9]+)/$', admin_views.RoundOverview.as_view(), name="finance.admin.round.overview"),
     url(r'^admin/year/(?P<year>[0-9]+)/$', admin_views.YearOverview.as_view(), name="finance.admin.year.overview"),
