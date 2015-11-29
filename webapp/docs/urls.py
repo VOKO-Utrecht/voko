247edc123ddc0eed96823aadf927d514b075b163
Rik van Achterberg <rik@byte.nl>
2015-11-29 15:39:23 +0100

    Change docs main url

--- webapp/docs/urls.py
+++ webapp/docs/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from .views import DocumentOverview, DocumentDownload
 
 urlpatterns = patterns('',
-    url(r'^all/$', DocumentOverview.as_view(), name="docs.document_overview"),
     url(r'^download/(?P<slug>[a-z0-9\-]+)/$', DocumentDownload.as_view(), name="docs.document_download"),
+    url(r'^', DocumentOverview.as_view(), name="docs.document_overview"),
 )
