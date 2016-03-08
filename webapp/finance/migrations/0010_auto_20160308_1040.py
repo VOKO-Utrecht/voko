77699edf6dfc26101d1be0ce1b8a3e1cf65daceb
Rik van Achterberg <rik@byte.nl>
2016-03-08 10:53:57 +0100

    Data migration to link all correct balance credits to their payment objects

---
+++ webapp/finance/migrations/0010_auto_20160308_1040.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def link_balances_to_payments(apps, _):
+    Balance = apps.get_model("finance", "Balance")
+    Payment = apps.get_model("finance", "Payment")
+
+    for p in Payment.objects.filter(succeeded=True):
+        print "======"
+        print "Payment %s, amount %s" % (p.id, p.amount)
+
+        # exceptions
+        if p.id == 44:
+            p.balance = Balance.objects.get(id=313)
+
+        if not p.balance:
+            try:
+                p.balance = Balance.objects.get(user=p.order.user,
+                                                amount=p.amount,
+                                                notes="Bestelling #%s contant betaald" % p.order.id)
+            except Balance.DoesNotExist:
+                p.balance = Balance.objects.get(user=p.order.user,
+                                                amount=p.amount,
+                                                notes="iDeal betaling voor bestelling #%s" % p.order.id)
+
+        p.save()
+        print "Linking to Balance: ID %s, AMOUNT %s, NOTES: %s" % (p.balance.id, p.balance.amount, p.balance.notes)
+
+    print "Done!"
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('finance', '0009_auto_20160304_1511'),
+    ]
+
+    operations = [
+        migrations.RunPython(link_balances_to_payments)
+    ]
