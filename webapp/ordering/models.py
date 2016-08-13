6e93e7b4b85e51d2406235897f17752fb920fb09
Rik van Achterberg <rik@rikva.nl>
2016-08-13 15:41:21 +0200

    Add optional 'notes' field to ProductStock

--- webapp/ordering/models.py
+++ webapp/ordering/models.py
@@ -431,6 +431,8 @@ class ProductStock(TimeStampedModel):
         (TYPE_ADDED, "Added"),
         (TYPE_LOST, "Lost")), default=TYPE_ADDED)
 
+    note = models.CharField(max_length=255, null=True, blank=True)
+
     class Meta:
         verbose_name = verbose_name_plural = "Productvoorraad"
 
