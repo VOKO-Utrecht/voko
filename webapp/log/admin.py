f0c358d2519d5fa220b30baaab5db6213fe96692
Rik van Achterberg <rik@rikva.nl>
2016-12-30 11:36:38 +0100

    Disable delete for logs

--- webapp/log/admin.py
+++ webapp/log/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from log.models import EventLog
+from vokou.admin import DeleteDisabledMixin
 
 
-class EventLogAdmin(admin.ModelAdmin):
+class EventLogAdmin(DeleteDisabledMixin, admin.ModelAdmin):
     list_display = ["created", "event", "operator", "user"]
     ordering = ("-id", )
     search_fields = ['event', 'operator__email', 'user__email']
