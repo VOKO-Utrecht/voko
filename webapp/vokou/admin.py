4ac34f392bb1001ce59ecbc168ec9a7440138725
Rik van Achterberg <rik@rikva.nl>
2017-01-03 13:17:39 +0100

    Add LogEntry admin

--- webapp/vokou/admin.py
+++ webapp/vokou/admin.py
@@ -1,5 +1,11 @@
 import unicodecsv
 from django.http import HttpResponse
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+from django.utils.html import escape
+from django.core.urlresolvers import reverse, NoReverseMatch
+
+from accounts.models import VokoUser
 
 
 class DeleteDisabledMixin(object):
@@ -46,3 +52,118 @@ def export_as_csv_action(description="Export selected objects as CSV file",
 
     export_as_csv.short_description = description
     return export_as_csv
+
+
+# The following code is adapted from https://djangosnippets.org/snippets/3009/
+
+action_names = {
+    ADDITION: 'Addition',
+    CHANGE:   'Change',
+    DELETION: 'Deletion',
+}
+
+
+class FilterBase(admin.SimpleListFilter):
+    def queryset(self, request, queryset):
+        if self.value():
+            dictionary = dict(((self.parameter_name, self.value()),))
+            return queryset.filter(**dictionary)
+
+
+class ActionFilter(FilterBase):
+    title = 'action'
+    parameter_name = 'action_flag'
+
+    def lookups(self, request, model_admin):
+        return action_names.items()
+
+
+class UserFilter(FilterBase):
+    """Use this filter to only show current users, who appear in the log."""
+    title = 'user'
+    parameter_name = 'user_id'
+
+    def lookups(self, request, model_admin):
+        return tuple((u.id, u.email)
+            for u in VokoUser.objects.filter(pk__in =
+                LogEntry.objects.values_list('user_id').distinct())
+        )
+
+
+class AdminFilter(UserFilter):
+    """Use this filter to only show current Superusers."""
+    title = 'admin'
+
+    def lookups(self, request, model_admin):
+        return tuple((u.id, u.email) for u in VokoUser.objects.filter(
+            is_superuser=True))
+
+
+class StaffFilter(UserFilter):
+    """Use this filter to only show current Staff members."""
+    title = 'staff'
+
+    def lookups(self, request, model_admin):
+        return tuple((u.id, u.email) for u in VokoUser.objects.filter(
+            is_staff=True))
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+    readonly_fields = [f.name for f in LogEntry._meta.get_fields()]
+
+    list_filter = [
+        UserFilter,
+        ActionFilter,
+        'content_type',
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+        'action_description',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def object_link(self, obj):
+        repr_ = escape(obj.object_repr)
+
+        try:
+            href = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj.__class__.__name__.lower()), args=[obj.object_id])
+            link = u'<a href="%s">%s</a>' % (href, repr_)
+        except NoReverseMatch:
+            link = repr_
+
+        return link if obj.action_flag != DELETION else repr_
+
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+
+    def queryset(self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
+
+    def action_description(self, obj):
+        return action_names[obj.action_flag]
+    action_description.short_description = 'Action'
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
