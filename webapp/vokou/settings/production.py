ed113a1b36ab3d9079aa2758f8f83233cde0bbad
Rik van Achterberg <rik@rikva.nl>
2017-01-06 15:31:54 +0100

    Change db usernames

--- webapp/vokou/settings/production.py
+++ webapp/vokou/settings/production.py
@@ -4,11 +4,11 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'vokou',                      # Or path to database file if using sqlite3.
-        'USER': 'vokou',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'voko',
+        'USER': 'voko',
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',
         }
     }
 
