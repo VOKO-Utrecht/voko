d6edafe6ae79185d16f1c7bec3114030d3e3c578
Rik van Achterberg <rik@rikva.nl>
2017-01-21 14:33:50 +0100

    Update runcrons.sh

--- webapp/runcrons.sh
+++ webapp/runcrons.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-cd /home/voko/vokou/webapp
+cd
 source production_secrets.sh
 
 source /home/voko/.virtualenvs/voko/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 
+cd /home/voko/voko/webapp
 exec python manage.py runcrons
