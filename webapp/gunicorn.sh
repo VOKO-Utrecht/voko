d1afa308ac029d914800665d6d05cd59cafc14c3
Rik van Achterberg <rik@rikva.nl>
2017-01-21 15:05:25 +0100

    Update gunicorn.sh

--- webapp/gunicorn.sh
+++ webapp/gunicorn.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+cd
 source ~/production_secrets.sh
 
 source /home/voko/.virtualenvs/voko/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 
+cd voko/webapp
 exec gunicorn vokou.wsgi:application -c gunicorn.conf.py
