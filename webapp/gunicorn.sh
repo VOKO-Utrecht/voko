df597b4e3126774135e1bd5655fea3912ee7ef6a
Rik van Achterberg <rik@rikva.nl>
2017-01-21 14:39:38 +0100

    Update helper scripts again

--- webapp/gunicorn.sh
+++ webapp/gunicorn.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source production_secrets.sh
+source ~/production_secrets.sh
 
 source /home/voko/.virtualenvs/voko/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
--- webapp/runcrons.sh
+++ webapp/runcrons.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-cd
-source production_secrets.sh
+source ~/production_secrets.sh
 
 source /home/voko/.virtualenvs/voko/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
