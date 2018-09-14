5b0e6da6db7356fe452717d398676d89b52be156
Rik van Achterberg <rik@programmable.nl>
2018-09-14 12:16:48 +0200

    Remove opbeat

--- webapp/vokou/settings/production.py
+++ webapp/vokou/settings/production.py
@@ -22,17 +22,3 @@ ADMINS = (
 ALLOWED_HOSTS = ("leden.vokoutrecht.nl", "dev.vokoutrecht.nl", "127.0.0.1")
 
 MOLLIE_API_KEY = os.environ["MOLLIE_API_KEY"]
-
-INSTALLED_APPS += [
-    "opbeat.contrib.django",
-]
-
-OPBEAT = {
-    "ORGANIZATION_ID": os.environ["OPBEAT_ORGANIZATION_ID"],
-    "APP_ID": os.environ["OPBEAT_APP_ID"],
-    "SECRET_TOKEN": os.environ["OPBEAT_SECRET"]
-}
-
-MIDDLEWARE_CLASSES = [
-    'opbeat.contrib.django.middleware.OpbeatAPMMiddleware',
-] + MIDDLEWARE_CLASSES
