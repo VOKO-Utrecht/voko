70e667d383c335f5a35490acd23e2a20e1d731fa
Peter Uithoven <peter@peteruithoven.nl>
2019-04-14 02:01:15 +0200

    Enable from email customization per email template
    Fixing: #39

--- webapp/mailing/helpers.py
+++ webapp/mailing/helpers.py
@@ -9,16 +9,18 @@ from mailing.models import MailTemplate
 def render_mail_template(template, **kwargs):
     body_tpl = Template(template.html_body)
     subject_tpl = Template(template.subject)
+    from_email_tpl = Template(template.from_email)
     context = Context(kwargs)
 
     rendered_subject = subject_tpl.render(context)
+    rendered_from_email = from_email_tpl.render(context)
     rendered_html_body = body_tpl.render(context)
 
     h2t = html2text.HTML2Text()
     h2t.body_width = 0
     rendered_plain_body = h2t.handle(rendered_html_body)
 
-    return rendered_subject, rendered_html_body, rendered_plain_body
+    return rendered_subject, rendered_html_body, rendered_plain_body, rendered_from_email
 
 
 def get_template_by_id(template_id):
@@ -29,10 +31,10 @@ def get_template_by_id(template_id):
             return MailTemplate(title="TEST")
 
 
-def mail_user(user, subject, html_body, plain_body):
+def mail_user(user, subject, html_body, plain_body, from_email):
     send_mail(subject=subject,
               message=plain_body,
-              from_email="VOKO Utrecht <info@vokoutrecht.nl>",
+              from_email=from_email if from_email else "VOKO Utrecht <info@vokoutrecht.nl>",
               recipient_list=["%s <%s>" % (user.get_full_name(), user.email)],
               html_message=html_body)
     log.log_event(user=user, event="Mail sent: %s" % subject, extra=html_body)
