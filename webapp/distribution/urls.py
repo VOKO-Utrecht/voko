b6261d1e9ca37ed0ac3e809e45773f065b61fde1
Peter Uithoven <peter@peteruithoven.nl>
2019-06-23 16:30:31 +0200

    Link to distribution schedule

--- webapp/distribution/urls.py
+++ webapp/distribution/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import Schedule, Shift
 
 urlpatterns = (
-    url(r'^schedule/$', Schedule.as_view(), name="schedule"),
+    url(r'^schedule/$', Schedule.as_view(), name="distribution-schedule"),
     url(r'^shift/(?P<slug>[-\w]+)/$', Shift.as_view(), name="shift")
 )
