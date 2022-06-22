46ff614678e50a8ef2a180f910caa18b63f82ed3
Marc <mvandongen@gmail.com>
2022-06-22 20:20:30 +0200

    Middleware

--- webapp/vokou/middleware.py
+++ webapp/vokou/middleware.py
@@ -2,5 +2,12 @@ from ordering.core import get_current_order_round
 
 
 class OrderRoundMiddleware(object):
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
         request.current_order_round = get_current_order_round()
+
+        response = self.get_response(request)
+        return response
