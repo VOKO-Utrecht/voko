5f1aa84694ab770e9097579b83411ce28781b6f6
Marc <mvandongen@gmail.com>
2022-06-22 20:43:31 +0200

    added dev postgresql config

--- webapp/vokou/settings/development.py
+++ webapp/vokou/settings/development.py
@@ -17,6 +17,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'voko',
+    #     'USER': 'vokouser',
+    #     'PASSWORD': 'vokouser',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
