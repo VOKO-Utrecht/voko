a71db1d45918d0271ad1629abe4e309b46d28e93
Marc <mvandongen@gmail.com>
2022-07-23 14:21:24 +0200

    Dockerize dev env

--- webapp/vokou/settings/development.py
+++ webapp/vokou/settings/development.py
@@ -28,10 +28,10 @@ DATABASES = {
     # }    
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME', 'postgres'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
         'PORT': 5432,
     }    
 }
