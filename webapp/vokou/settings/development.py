16fda5e323d6804cdbb65c8dc3bb038cffa1a084
Marc <mvandongen@gmail.com>
2022-07-23 14:21:24 +0200

    Dockerize dev env

--- webapp/vokou/settings/development.py
+++ webapp/vokou/settings/development.py
@@ -26,9 +26,13 @@ DATABASES = {
     #     'PORT': '5432',
     # }    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }    
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
