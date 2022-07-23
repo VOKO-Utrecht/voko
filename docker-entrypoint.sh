3a4939335c41b317a3fe582df2927d156b05a884
Marc <mvandongen@gmail.com>
2022-07-23 14:21:24 +0200

    Dockerize dev env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --settings=vokou.settings.development
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000 --settings=vokou.settings.development 
+python manage.py runserver 0.0.0.0:8000 --settings=vokou.settings.development
