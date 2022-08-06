f1ba80ca3513bba4e63f339aea44558acf0aa446
Marc <mvandongen@gmail.com>
2022-08-06 11:29:25 +0200

    sqlite for testing

--- webapp/vokou/settings/development.py
+++ webapp/vokou/settings/development.py
@@ -19,13 +19,9 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'voko',
-    #     'USER': 'vokouser',
-    #     'PASSWORD': 'vokouser',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }    
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME'),
