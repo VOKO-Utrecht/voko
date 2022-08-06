6e72d0988359fec0552093c8cf009855326f941f
Marc <mvandongen@gmail.com>
2022-08-06 11:34:21 +0200

    added comment for easy sqlite development

--- webapp/vokou/settings/development.py
+++ webapp/vokou/settings/development.py
@@ -17,10 +17,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME'),
