454be5900bc70a0597b12f0a8dcaf82dce09defa
mgvd <mvandongen@gmail.com>
2023-01-13 13:42:17 +0100

    update allowed hosts

--- webapp/vokou/settings/production.py
+++ webapp/vokou/settings/production.py
@@ -18,6 +18,6 @@ ADMINS = (
     ("Voko Utrecht", os.getenv('ADMIN_EMAIL', "info@vokoutrecht.nl")),
 )
 
-ALLOWED_HOSTS = ("leden.vokoutrecht.nl", "dev.vokoutrecht.nl", "127.0.0.1")
+ALLOWED_HOSTS = ("ldn.vokoutrecht.nl", "leden.vokoutrecht.nl", "acc.vokoutrecht.nl", "dev.vokoutrecht.nl", "127.0.0.1")
 
 MOLLIE_API_KEY = os.environ["MOLLIE_API_KEY"]
