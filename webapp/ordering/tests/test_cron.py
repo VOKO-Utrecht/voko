d8696bda4bf75647e48fb2908cc64401b769000b
mgvd <mvandongen@gmail.com>
2023-02-15 17:16:22 +0100

    python3 csv only accepts strings (#260)

--- webapp/ordering/tests/test_cron.py
+++ webapp/ordering/tests/test_cron.py
@@ -1,9 +1,9 @@
 from vokou.testing import VokoTestCase
-from ordering.cron import SendPickupReminders
+from ordering.cron import SendPickupReminders, MailOrderLists
 from datetime import datetime, timedelta
 from pytz import UTC
 from mock import patch
-from ordering.tests.factories import OrderRoundFactory
+from ordering.tests.factories import OrderFactory, OrderProductFactory, OrderRoundFactory, ProductFactory
 
 
 class TestPickupReminderJob(VokoTestCase):
@@ -49,3 +49,22 @@ class TestPickupReminderJob(VokoTestCase):
 
             SendPickupReminders.do(self)
             self.assertFalse(mock_mail.called)
+
+    def test_mail_order_list(self):
+        with patch("django.core.mail.EmailMultiAlternatives.send") as \
+                mock_mail:
+            now = datetime.now(tz=UTC)
+            self.order_round = OrderRoundFactory(
+                open_for_orders=now - timedelta(days=4),
+                closed_for_orders=now - timedelta(days=1),
+                collect_datetime=now + timedelta(hours=1))            
+            product = ProductFactory(order_round=self.order_round,
+                                 maximum_total_order=10)
+            order1 = OrderFactory(order_round=self.order_round,
+                                finalized=True,
+                                paid=True)
+            OrderProductFactory(order=order1, product=product, amount=10)
+
+
+            MailOrderLists.do(self)
+            self.assertTrue(mock_mail.called)
