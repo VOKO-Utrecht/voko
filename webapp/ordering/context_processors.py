e22599cb89120c345f6958fc187ac30c053001cb
Berend Veldkamp <7160755+bveldkamp@users.noreply.github.com>
2023-10-09 12:02:45 +0200

    Removed trailing whitespace

--- webapp/ordering/context_processors.py
+++ webapp/ordering/context_processors.py
@@ -2,7 +2,7 @@ from ordering.models import PickupLocation
 
 
 def pickup_locations(request):
-    locations = PickupLocation.objects.all()    
+    locations = PickupLocation.objects.all()
     return {
         'pickup_locations': locations
     }
