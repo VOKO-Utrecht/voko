b7b9ea20758431c7a9971cfb9b7f04f5b73bbf9a
Berend Veldkamp <7160755+bveldkamp@users.noreply.github.com>
2023-10-09 11:44:31 +0200

    Added context processor for pickup locations

---
+++ webapp/vokou/context_processors.py
@@ -0,0 +1,8 @@
+from ordering.models import PickupLocation
+
+
+def pickup_locations(request):
+    locations = PickupLocation.objects.all()    
+    return {
+        'pickup_locations': locations
+    }
