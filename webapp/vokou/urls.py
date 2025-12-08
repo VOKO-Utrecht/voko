83d85026ef6c47c4780e9ecfe00db7f9f35bcba0
Tobias Cadee <tobias.cadee@ticketswap.com>
2025-12-08 22:29:12 +0100

    fix linting, fix test running

--- webapp/vokou/urls.py
+++ webapp/vokou/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.contrib import admin
 import accounts.urls
 import docs.urls
