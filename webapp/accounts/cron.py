7b04d1ab3911af3d33f86ef214b691e24358e4a0
Berend Veldkamp <7160755+bveldkamp@users.noreply.github.com>
2026-01-05 13:16:20 +0100

    Catch broader exception

--- webapp/accounts/cron.py
+++ webapp/accounts/cron.py
@@ -19,17 +19,19 @@ class DeleteInactiveAccounts(CronJobBase):
     code = "accounts.delete_inactive_accounts"
 
     def delete_inactive_users(self, model):
-        six_months_ago = datetime.now() - timedelta(weeks=26)
-        inactive_users = model.objects.filter(Q(created__lt=six_months_ago) & Q(can_activate=False))
+        # Get non-activated accounts older than half a year
+        half_year_ago = datetime.now() - timedelta(weeks=26)
+        inactive_users = model.objects.filter(Q(created__lt=half_year_ago) & Q(can_activate=False))
 
         # Delete users one by one to prevent a single failure to stop the entire transaction
         for user in inactive_users:
             try:
                 log.log_event(user=user, event=f"Deleting inactive useraccount {user.id}")
                 model.delete(user)
-            except DatabaseError as e:
+            except Exception as e:
                 log.log_event(user=user, event=f"Could not delete inactive useraccount {user.id}", extra=str(e))
 
     def do(self):
+        # Apparently, there are non-activated accounts that were anonimised nevertheless
         for model in (VokoUser, SleepingVokoUser):
             self.delete_inactive_users(model)
