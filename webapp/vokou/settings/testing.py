f3c6a519ffd916842b7ff2f39f1f35d65e00d5c4
Berend Veldkamp <7160755+bveldkamp@users.noreply.github.com>
2026-02-09 16:35:31 +0100

    Fix wrong merge

--- webapp/vokou/settings/testing.py
+++ webapp/vokou/settings/testing.py
@@ -8,20 +8,8 @@ PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),  # noqa: F405
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
 DEBUG = False
-
-
-# https://stackoverflow.com/questions/25161425/disable-migrations-when-running-unit-tests-in-django-1-7
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return
-
-
-MIGRATION_MODULES = DisableMigrations()
